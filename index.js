71cd4ed48639f17b439b7b246a14b455ab2ce00b
irfanalfth <irfanalfth@gmail.com>
2022-10-26 10:49:18 +0700

    baru

--- index.js
+++ index.js
@@ -1,58 +1,58 @@
-const fs = require("fs-extra");
+var mysql = require("mysql");
 const readline = require("readline-sync");
-const prompt = require("prompt-sync");
 var clc = require("cli-color");
 
-const file = fs.readFileSync("./tempat.txt", "utf-8");
-const splitFile = file.split("\r\n");
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "tour",
+});
 
-console.log("\n");
-console.log(
-  clc.greenBright.bold(splitFile.length - 1) +
-    clc.yellow(` data akan di input secara berurutan.\n`) +
-    clc.red(`Pastikan anda input data dengan benar!\n`) +
-    clc.blueBright(`Jika anda salah input maka mengulang dari awal`)
-);
+con.connect(function (err) {
+  if (err) throw err;
 
-for (let i = 0; i < splitFile.length; i++) {
-  let mysql = require("mysql");
-  let config = require("./config.js");
-  let connection = mysql.createConnection(config);
+  let sql = `SELECT * FROM tour WHERE latitude = 0 AND longitude = 0 LIMIT 2`;
 
-  var files = fs.readFileSync("./tempat.txt", "utf-8");
-  var lines = files.split("\r\n");
-  lines.splice(0, 1);
+  con.query(sql, (err, result) => {
+    if (err) throw err;
 
-  const tempat = splitFile[i].split("|")[0];
-  const id = splitFile[i].split("|")[1];
-
-  console.log("\n");
-  console.log(
-    clc.blueBright.underline(`Cari ${clc.red.bold(tempat)} di google maps`)
-  );
-  console.log(clc.yellow(`Latitude dan Longitude :`));
-
-  do {
-    var input = readline.question("=> ");
-  } while (!input);
-
-  let ll = input.replace(/ /g, "").split(",", 2);
-
-  let sql = `UPDATE tour SET latitude = '${ll[0]}',longitude ='${ll[1]}' WHERE tourCode = ${id};`;
-
-  let data = [false, 1];
-
-  connection.query(sql, data, (error, results, fields) => {
-    if (error) {
-      return console.error(error.message);
-    }
-    console.log(`\n`);
-    console.log(clc.whiteBright(`Tempat    : ` + clc.blueBright(tempat)));
-    console.log(clc.whiteBright(`Latitude  : ` + clc.yellowBright(ll[0])));
+    console.log("\n");
     console.log(
-      clc.whiteBright(`Longitude : ` + clc.yellowBright(ll[1])) + `\n`
+      clc.greenBright.bold(result.length) +
+        clc.yellow(` data akan di input secara berurutan.\n`) +
+        clc.red(`Pastikan anda input data dengan benar!\n`) +
+        clc.blueBright(`Jika anda salah input maka mengulang dari awal`)
     );
-  });
 
-  connection.end();
-}
+    result.forEach((a) => {
+      console.log("\n");
+      console.log(
+        clc.blueBright.underline(`Cari ${clc.red.bold(a.name)} di google maps`)
+      );
+      console.log(clc.yellow(`Latitude dan Longitude :`));
+
+      do {
+        var input = readline.question("=> ");
+      } while (!input);
+
+      let ll = input.replace(/ /g, "").split(",", 2);
+
+      let data = [false, 1];
+
+      let sql2 = `UPDATE tour SET latitude = '${ll[0]}',longitude ='${ll[1]}' WHERE tourCode = ${a.tourCode};`;
+
+      con.query(sql2, data, (error, results) => {
+        if (error) {
+          return console.error(error.message);
+        }
+        console.log(`\n`);
+        console.log(clc.whiteBright(`Tempat    : ` + clc.blueBright(a.name)));
+        console.log(clc.whiteBright(`Latitude  : ` + clc.yellowBright(ll[0])));
+        console.log(
+          clc.whiteBright(`Longitude : ` + clc.yellowBright(ll[1])) + `\n`
+        );
+      });
+    });
+  });
+});
